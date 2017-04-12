c2d286904ec6fdd97f2bc6603f8fb105a61427f8
CHUKIN <dmitriyatamanchuk@gmail.com>
2017-04-12 22:04:36 +0300

    Педставление запроса на склад

--- AccountingSite/Controllers/ChiefController.cs
+++ AccountingSite/Controllers/ChiefController.cs
@@ -17,9 +17,31 @@ namespace AccountingSite.Controllers
             return View(db.Orders.Where(i=>i.From.Login==User.Identity.Name));
         }
 
+        [HttpGet]
         public ActionResult StoreRequest()
         {
             return View(db);
+        }
+
+        [HttpPost]
+        public ActionResult StoreRequest(string[] counts,string[] items, string text, string to)
+        {
+            Order order = new Order()
+            {
+                From = db.Employees.Where(i => i.Name == User.Identity.Name).FirstOrDefault(),
+                To = db.Employees.Where(i => i.Name == db.Employees.Where(j => j.Name == to).FirstOrDefault().Name).FirstOrDefault(),
+                Text = text,
+                Date = DateTime.Now,
+                Status = db.Statuses.Where(i => i.Name == "SendToStore").FirstOrDefault()
+            };
+
+            db.Orders.Add(order);
+
+
+
+
+            db.SaveChanges();
+            return Content(order.From.Name);
         }
 
         public ActionResult ItemExtradition()
